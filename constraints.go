1b14aa66168d80ec33712004bbbd6b349e604417
Thorsten Riechers <99222474+thorstenrie@users.noreply.github.com>
2023-02-12 19:44:10 +0100

    Update comments

--- constraints.go
+++ constraints.go
@@ -3,32 +3,32 @@
 // that can be found in the LICENSE file.
 package lpstats
 
-// number constraints types to all number types
+// Number constraints types to all number types
 type Number interface {
 	Integer | Floating
 }
 
-// signed constraints types to signed number types
+// Signed constraints types to signed number types
 type Signed interface {
 	Floating | Sinteger
 }
 
-// integer constraints types to signed and unsigned integer types
+// Integer constraints types to signed and unsigned integer types
 type Integer interface {
 	Sinteger | Uinteger
 }
 
-// sinteger constraints types to signed integer types
+// Sinteger constraints types to signed integer types
 type Sinteger interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
-// uinteger constraints types to unsigned integer types
+// Uinteger constraints types to unsigned integer types
 type Uinteger interface {
 	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
 }
 
-// floating constraints types to floating types
+// Floating constraints types to floating types
 type Floating interface {
 	~float32 | ~float64
 }
